8dcbaca50ec9dead903e58951489d03db54eae11
Dan <danielmcollins1996@gmail.com>
2022-08-07 23:19:40 -0600

    added default export

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-console.log("heyheyheyh")
+console.log("Hey From Deno")
 
 console.log(await Deno.readTextFile('test.json'))
 
 console.log('Run the Command in the README.md to run this in watch mode')
+
+export { }
